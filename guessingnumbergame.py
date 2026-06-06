b8be10f6e2e5cdb7596b5201c2df7ea8dad787e8
giddeladewoye20-droid <giddeladewoye20@gmail.com>
2026-06-06 18:00:41 +0100

    Add files via upload

---
+++ guessingnumbergame.py
@@ -0,0 +1,17 @@
+import tkinter 
+screen=tkinter.Tk()
+
+
+
+screen.geometry("670x670")
+screen.title("PAQUETA CRISPS")
+label1=tkinter.Label(screen,text="Whats your name? ")
+label2=tkinter.Label(screen,text=)
+entry1=tkinter.Entry(screen)
+button1=tkinter.Button(screen,text="Ok")
+button2=tkinter.Button(screen,text="Exit")
+
+label1.pack()
+label2.pack()
+button1.pack()
+button2.pack
