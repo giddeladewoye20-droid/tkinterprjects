5eb7c4cccb9314f94e04fde7e95b482cf3af6d31
giddeladewoye20-droid <giddeladewoye20@gmail.com>
2026-07-12 17:58:20 +0100

    Add files via upload

--- mesmerizer.py
+++ mesmerizer.py
@@ -1,4 +1,5 @@
-import tkinter 
+import tkinter
+import tkinter.filedialog 
 screen=tkinter.Tk()
 
     
@@ -6,15 +7,37 @@ screen=tkinter.Tk()
 screen.geometry("500x200")
 screen.title("PAQUETA CRISPS") 
 
+def openfunc():
+   ayrmimightjsbunhim=tkinter.filedialog.askopenfile()
+   if ayrmimightjsbunhim != None:
+       slidenyuckhim=ayrmimightjsbunhim.readlines()
+       listbox.delete(0,tkinter.END)
+       for item in slidenyuckhim:
+           listbox.insert(tkinter.END,item)
 
-label1=tkinter.Label(screen)
+
+def deletefunc():
+   index=listbox.curselection()
+   listbox.delete(index)
+
+def savefunc():
+    dkpassmethesuttin=tkinter.filedialog.asksaveasfile()
+    if dkpassmethesuttin != None:
+        for item in listbox.get(0,tkinter.END):
+            print(item,file=dkpassmethesuttin)
+
+def addfunc():
+   addresponse=entry1.get()
+   if addresponse !="":
+       listbox.insert(tkinter.END,addresponse)
+       entry1.delete(0,tkinter.END)
+
+
 entry1=tkinter.Entry(screen)
-label2=tkinter.Label(screen)
-entry2=tkinter.Entry(screen)
-button1=tkinter.Button(screen,text="delete")
-button2=tkinter.Button(screen,text="open")
-button3=tkinter.Button(screen,text="save")
-button4=tkinter.Button(screen,text="add")
+button1=tkinter.Button(screen,text="delete",command=deletefunc)
+button2=tkinter.Button(screen,text="open",command=openfunc)
+button3=tkinter.Button(screen,text="save",command=savefunc)
+button4=tkinter.Button(screen,text="add",command=addfunc)
 listbox=tkinter.Listbox(screen)
 
 button1.grid(row=1,column=2)
